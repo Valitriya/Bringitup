f4d7a30ebc4691c7d80fd33496421a0a9d151b10
Valensi <vallitriya@gmail.com>
2023-05-21 22:46:18 +0300

    start creating forms

---
+++ src/js/modules/forms.js
@@ -0,0 +1,46 @@
+export default class Form {
+    constructor(forms) {
+        this.forms = document.querySelectorAll(forms);
+        this.inputs = document.querySelectorAll('input');
+        this.message = {
+            loading: 'Загрузка...',
+            success: 'Спасибо! Скоро мы с вами свяжемся!',
+            failure: 'Что-то пошло не так...'
+        };
+        this.path = 'assets/question.php';
+    }
+
+    clearInputs() {
+        this.inputs.forEach(item => {
+            item.value = '';
+        });
+    }
+
+    checkMailInputs(){
+        const mailInputs = document.querySelectorAll('[type="email"]');
+    
+        mailInputs.forEach(input => {
+            input.addEventListener('keypress', function(e) {
+                if (e.key.match(/[^a-z 0-9 @ \.]/ig)) {
+                    e.preventDefault();
+                }
+            });
+        });
+    }
+
+    initMask() {
+
+        let setCursorPosition = (pos, elem) => {
+            elem.focus();
+            
+            if (elem.setSelectionRange) {
+                elem.setSelectionRange(pos, pos);
+            } else if (elem.createTextRange) {
+                let range = elem.createTextRange();
+
+                range.collapse(true);
+                range.moveEnd('character', pos);
+                range.moveStart('character', pos);
+                range.select();
+            }
+        };
