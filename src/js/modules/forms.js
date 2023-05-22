5760856d7990ab7e5601141b35aa1065469eae55
Valensi <vallitriya@gmail.com>
2023-05-22 21:48:16 +0300

    add  forms

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -44,3 +44,44 @@ export default class Form {
                 range.select();
             }
         };
+        function createMask(event) {
+            let matrix = '+1 (___) ___-____',
+                i = 0,
+                def = matrix.replace(/\D/g, ''),
+                val = this.value.replace(/\D/g, '');
+
+            if (def.length >= val.length) {
+                val = def;
+            }
+
+            this.value = matrix.replace(/./g, function(a) {
+                return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? '' : a;
+            });
+
+            if (event.type === 'blur') {
+                if (this.value.length == 2) {
+                    this.value = '';
+                }
+            } else {
+                setCursorPosition(this.value.length, this);
+            }
+        }
+
+        let inputs = document.querySelectorAll('[name="phone"]');
+
+        inputs.forEach(input => {
+            input.addEventListener('input', createMask);
+            input.addEventListener('focus', createMask);
+            input.addEventListener('blur', createMask);
+        });
+    }
+
+    async postData(url, data) {
+        let res = await fetch(url, {
+            method: "POST",
+            body: data
+        });
+    
+        return await res.text();
+    }
+}
