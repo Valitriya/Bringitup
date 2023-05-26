52cfb88987caa601a3af8e9d20cc8ae1b29cdb5e
Valensi <vallitriya@gmail.com>
2023-05-26 23:58:56 +0300

    fixed project

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,8 @@
 import MainSlider from "./modules/slider/slider-main";
-import MiniSlider from "./modules/slider/slider-mini";
 import VideoPlayer from "./modules/playVideo";
-import Difference from "./modules/difference";
 
 window.addEventListener('DOMContentLoaded', () => {
-    const slider = new MainSlider({btns: ".next", container: ".page"});
+    const slider = new MainSlider({btns: ".next", page: ".page"});
     slider.render();
 
     const showUpSlider = new MiniSlider({
@@ -36,7 +34,4 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const player = new VideoPlayer('.showup .play', '.overlay');
     player.init();
-
-    new Difference('.officerold', '.officernew', '.officer__card-item').init();
-    
 });
