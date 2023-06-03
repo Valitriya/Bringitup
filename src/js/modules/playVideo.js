9746730d95a849aa0c56fbd938dccf7596d7f707
Valensi <vallitriya@gmail.com>
2023-06-03 23:11:52 +0300

    done  module play

--- src/js/modules/playVideo.js
+++ src/js/modules/playVideo.js
@@ -7,22 +7,33 @@ export default class VideoPlayer {
     }
 
     bindTriggers() {
-        this.btns.forEach(btn => {
-            btn.addEventListener('click', () => {
-                if (document.querySelector('iframe#frame')) {
-                    this.overlay.style.display = 'flex';
-                    if(this.path !== btn.getAttribute('data-url')){
-                       this.path = btn.getAttribute('data-url');
-                       this.player.loadVideoById({videoId: this.path});
-                    }
-                } else {
-                    this.path = btn.getAttribute('data-url');
+        this.btns.forEach((btn, i) => {
+           try{
+            const blockedElem = btn.closest('.module__video-item').nextElementSibling; 
+            if(i % 2 == 0){
+                blockedElem.setAttribute('data-disabled', 'true')
+            }
+           }catch(e){}
 
-                    this.createPlayer(this.path);
-                }
-            });
+                btn.addEventListener('click', () => {
+                    if(!btn.closest('.module__video-item') || btn.closest('.module__video-item').getAttribute('data-disabled') !== 'true'){
+                        this.activeBtn = btn;
+
+                    if (document.querySelector('iframe#frame')) {
+                        this.overlay.style.display = 'flex';
+                        if(this.path !== btn.getAttribute('data-url')){
+                           this.path = btn.getAttribute('data-url');
+                           this.player.loadVideoById({videoId: this.path});
+                        }
+                    } else {
+                        this.path = btn.getAttribute('data-url');
+    
+                        this.createPlayer(this.path);
+                    }
+                };     
         });
-    }
+    });
+}
 
     bindCloseBtn() {
         this.close.addEventListener('click', () => {
@@ -45,6 +56,7 @@ export default class VideoPlayer {
     }
 
     onPlayerStateChange(state){
+       try{
         const blockedElem = this.activeBtn.closest('.module__video-item').nextElementSibling;
         const playBtn = this.activeBtn.querySelector('svg').cloneNode(true);
 
@@ -57,9 +69,10 @@ export default class VideoPlayer {
                 blockedElem.querySelector('.play__text').classList.remove('attention');
                 blockedElem.style.opacity = 1;
                 blockedElem.style.filter = 'none';
-
+                blockedElem.getAttribute('data-disabled', 'false');
             }
         }
+       }catch(e){}
     }
 
     init() {
